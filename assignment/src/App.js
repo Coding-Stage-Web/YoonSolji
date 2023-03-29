42d83212d7c81116094dba974779e8acc2ca9c8e
solarthenomad <oakikim@ewhain.net>
2023-03-29 19:16:00 +0900

    커밋

---
+++ assignment/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import week1 from "./week1";
+
+function App() {
+  return (
+    <div>
+      <h1>Assignment1</h1>
+      <p>Life cycle Method</p>
+      <Assignment1 prop_value="생명주기함수" />
+    </div>
+  );
+}
+
+export default App;
