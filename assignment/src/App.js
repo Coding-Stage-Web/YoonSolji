2446317fca8a05e0c84a70b5b6ef529ea58fb6f8
solarthenomad <oakikim@ewhain.net>
2023-04-07 00:56:10 +0900

    커밋

--- assignment/src/App.js
+++ assignment/src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
-import week1 from "./week1";
+import Week2 from "./week2";
+
 
 function App() {
   return (
-    <div>
-      <h1>Assignment1</h1>
-      <p>Life cycle Method</p>
-      <Assignment1 prop_value="생명주기함수" />
+    <div>    
+      <Week2/>      
     </div>
+    
   );
 }
 
